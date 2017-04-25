91b25aeb0f4adac4a5eb0702966eb95a4f3efe49
Dan Kreiger <dan@dankreiger.com>
2017-04-25 19:44:52 +0200

    More organization

--- lib/puppy_money/money.rb
+++ lib/puppy_money/money.rb
@@ -5,21 +5,21 @@ require_relative 'rate_api'
 class Money
   attr_reader :amount, :base_currency
 
+  # use options for custom currency conversion
   def initialize amount, base_currency, options={}
-    validate_input(
-      amount: amount,
-      base_currency: base_currency,
-      options: options
-    )
+    validate_input amount, base_currency, options
   end
 
 
   private
 
-  def validate_input(**args)
-    raise ArgumentError, "Amount must be a number" unless args[:amount].is_a? Numeric
-    if !CURRENCIES.keys.map(&:to_s).include?(args[:base_currency])
-      raise ArgumentError, " `#{args[:base_currency]}` is invalid (currency symbol must be a 3 character string)"
-    end
+  def validate_input(amount, base_currency, options)
+    raise ArgumentError, "Amount must be a number" unless amount.is_a? Numeric
+    raise ArgumentError, base_currency_error(base_currency) if options.empty? && !RateApi.fetch_symbols.include?(base_currency)
+    raise ArgumentError, base_currency_error(base_currency) if !options.empty? && base_currency.try(:upcase) && !Currencies.abbreviations.include?(base_currency.try(:upcase))
+  end
+
+  def base_currency_error(base_currency)
+    " `#{base_currency}` is an invalid (currency abbreviation must be a recognized 3 character string)"
   end
 end
--- spec/puppy_money/money_spec.rb
+++ spec/puppy_money/money_spec.rb
@@ -15,6 +15,7 @@ describe Money do
 
     context "amount" do
       it "must be numerical" do
+        binding.pry
         # invalid amount
         strings.each    { |str|  expect{ Money.new str,  'EUR' }.to raise_error ArgumentError, amount_error }
         booleans.each   { |bool| expect{ Money.new bool, 'EUR' }.to raise_error ArgumentError, amount_error }
@@ -37,9 +38,10 @@ describe Money do
         arrays.each   { |arr|  expect{ Money.new 50, arr  }.to raise_error  ArgumentError }
         hashes.each   { |hash| expect{ Money.new 50, hash }.to raise_error  ArgumentError }
 
-        # valid base_currency
-        currencies.each { |currency| expect{ Money.new 50, currency }.to_not raise_error }
+        # valid base_currency for automatic currency conversion
         api_currencies.each { |currency| expect{ Money.new 50, currency }.to_not raise_error }
+        # valid base_currency for custom conversion
+        currencies.each { |currency| expect{ Money.new 50, currency, {fake_currency: '0.4562'} }.to_not raise_error }
       end
     end
   end
